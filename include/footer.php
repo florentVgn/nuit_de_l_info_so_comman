9c69615e97a47263497d8dc3f222878580c7ef86
destraic <charlotte.destrait@iut-valence.fr>
2016-12-02 00:15:26 +0100

    centrage

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,6 @@
 <footer class="footer col-lg-12">
-  <div class="col-lg-6"><img class="footer-nuitinfo " src="img/nuit_info.png"> </div>
-  <p class="footer-copyright col-lg-offset-6">Copyright © 2016 - SoComman</p>
-
+  <div class="col-lg-4"><img class="footer-nuitinfo " src="img/nuit_info.png"> </div>
+  <p class="footer-copyright col-lg-4">Copyright © 2016 - SoComman</p>
 
 
 </footer>
