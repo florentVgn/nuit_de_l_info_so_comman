d27422019587541dd3a657c9b59cb551ba0c8838
Maxime <maxime.ambry@gmail.com>
2016-12-02 00:12:04 +0100

    menu
    menu

--- carte.php
+++ carte.php
@@ -20,6 +20,9 @@ include_once 'app/bootstrap.inc.php';
     <!--FontAwesome -->
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 
+    <!--CSS -->
+    <link rel="stylesheet" href="style/style.css">
+
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -28,7 +31,38 @@ include_once 'app/bootstrap.inc.php';
     <![endif]-->
   </head>
   <body>
-    <h1><i class="fa fa-print" aria-hidden="true"></i>Ici on met la carte</h1>
+    <nav class="navbar navbar-default menu" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="home.php">
+            <img alt="WIIO" src="img/logo.jpg" height="20vh" style="margin-left: 10px;">
+          </a>
+          <a class="navbar-brand" href="index.php">
+            <p style="margin-left: 10px; font-family: 'Josefin Sans', sans-serif; color: white; height: 20px; line-height: 20px;">Refugees Assistant</p>
+          </a>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Actualité</a></li>
+            <li class="active"><a href="#">Ressources</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Droite</a></li>
+
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+    <h1><i class="fa fa-print" aria-hidden="true"></i>Selectionnez un département</h1>
     <a href="homeRefugie.php">Si on clique sur un lieu</a>
 
 
