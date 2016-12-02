20198f0ade1d61e2c905b5d5fecee1b9ff1587d3
destraic <charlotte.destrait@iut-valence.fr>
2016-12-02 02:17:38 +0100

    weshalor

--- include/footer.php
+++ include/footer.php
@@ -1,8 +1,10 @@
 <footer class="footer col-lg-12">
-  <div class="col-lg-1"><img class="footer-chicken_run " src="img/chicken_run.jpg"> </div>
-  <div class="col-lg-1"><img class="footer-tacos_n_co " src="img/tacos_n_co.jpg"> </div>
-  <div class="col-lg-1"><img class="footer-millenium " src="img/millenium.PNG"> </div>
-  <p class="footer-copyright col-lg-9">Copyright © 2016 - SoComman</p>
+  <div class="col-lg-6">
+  <div class="col-lg-2"><img class="footer-chicken_run " src="img/chicken_run.jpg"> </div>
+  <div class="col-lg-2"><img class="footer-tacos_n_co " src="img/tacos_n_co.jpg"> </div>
+  <div class="col-lg-2"><img class="footer-millenium " src="img/millenium.PNG"> </div>
+  </div>
+  <p class="footer-copyright col-lg-6">Copyright © 2016 - SoComman</p>
 </footer>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
