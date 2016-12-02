ae50e10689edbf97f3f5bd67ee2f37b11b466846
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2016-12-02 05:56:27 +0100

    fsdfsdfsdf

--- include/menuIndex.php
+++ include/menuIndex.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default menu" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="home.php">
+      <a class="navbar-brand" href="index.php">
         <img alt="WIIO" src="img/logo.jpg" height="20vh" style="margin-left: 10px;">
       </a>
       <a class="navbar-brand" href="index.php">
@@ -19,7 +19,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="navbar">
       <ul class="nav navbar-nav">
-  
+
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
--- include/menuOrga.php
+++ include/menuOrga.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default menu" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="home.php">
+      <a class="navbar-brand" href="index.php">
         <img alt="WIIO" src="img/logo.jpg" height="20vh" style="margin-left: 10px;">
       </a>
       <a class="navbar-brand" href="index.php">
