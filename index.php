b645d6e91f35c93468b31cdfbe4ad67a6c59fa39
becharib <bilal.bechari@iut-valence.fr>
2016-12-02 02:17:53 +0100

    index.php

--- index.php
+++ index.php
@@ -12,33 +12,39 @@ include_once 'app/bootstrap.inc.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Refugees Assistance</title>
 
     <!-- Bootstrap -->
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <link rel="stylesheet" href="style/style.css">
-
     <!--FontAwesome -->
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script type="text/javascript">
+      $(window).load(function() {
+        $('#exampleModal').modal('show');
+        
+      });
+    </script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
   </head>
   <body>
     <h1><i class="fa fa-print" aria-hidden="true"></i>Bonjour</h1>
     <a href="connexion.php">Organisateur</a>
     <a href="carte.php">Refugié</a>
+    <div class="modal fade bs-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          
+        </div>
+      </div>
+    </div>
+    
 
 
 
 
-
-
-    <?php include("include/footer.php");?>
   </body>
 </html>
