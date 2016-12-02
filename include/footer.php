7b822b819d1db8053d0e9150ac97d8527130fcb6
Maxime <maxime.ambry@gmail.com>
2016-12-02 02:00:36 +0100

    synchr

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,10 @@
 <footer class="footer col-lg-12">
-  <div class="col-lg-4"><img class="footer-nuitinfo " src="img/chicken_run.jpg"> </div>
-  <p class="footer-copyright col-lg-4">Copyright © 2016 - SoComman</p>
+  <div class="col-lg-3"><img class="footer-chicken_run " src="img/chicken_run.jpg"> </div>
+  <div class="col-lg-3"><img class="footer-tacos_n_co " src="img/tacos_n_co.jpg"> </div>
+  <p class="footer-copyright col-lg-">Copyright © 2016 - SoComman</p>
 </footer>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
