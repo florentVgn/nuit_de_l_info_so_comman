c75378935f31baa509038708273356b077471eed
Nicolas Jourdan <nicolas.jourdan.c@gmail.com>
2016-12-02 01:21:26 +0100

    2eme co

--- app/bootstrap.inc.php
+++ app/bootstrap.inc.php
@@ -4,3 +4,36 @@ $ZonesDAO = new ZonesDAO(MaBD::getInstance());
 $InscritsDAO = new InscritsDAO(MaBD::getInstance());
 
 session_start();
+
+//check les droits pour les pages
+function isGranted($userID)
+{
+  global $inscritsDAO;
+  $user = $inscritsDAO->getOne($userID);
+
+  if (strpos($_SERVER['REQUEST_URI'], 'admin.php'))
+  {
+    if (!($user['role'] =='ROLE_ADMINISTRATEUR'))
+    {
+
+        header("Location: ../web/connexion.php");
+    }
+  }
+
+  if (strpos($_SERVER['REQUEST_URI'], 'homeOrga.php'))
+  {
+    if (!($user['role'] =='ROLE_ADMINISTRATEUR') && !($user['role']=='ROLE_ORGANISTAEUR'))
+    {
+
+        header("Location: ../web/connexion.php");
+    }
+  }
+
+  if(strpos($_SERVER['REQUEST_URI'], 'connexion.php'))
+  {
+    if($user['role']=='ROLE_ADMINISTRATEUR'){
+      header('Location: ../web.admin.php');
+    }elseif($user['role']=='ROLE_ORGANISTAEUR'){
+      header('Location: ../web/homeOrga.php');
+    }
+  }
