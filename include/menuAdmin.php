50f3130a065ded8c4400ab693ec8135a4b5b5670
Maxime <maxime.ambry@gmail.com>
2016-12-02 03:02:40 +0100

    Add index page

--- include/menuAdmin.php
+++ include/menuAdmin.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default menu" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="home.php">
+      <a class="navbar-brand" href="index.php">
         <img alt="WIIO" src="img/logo.jpg" height="20vh" style="margin-left: 10px;">
       </a>
       <a class="navbar-brand" href="index.php">
@@ -19,8 +19,8 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="navbar">
       <ul class="nav navbar-nav">
-        <li class="lien-menu lien-actif"><a href="#">Accueil</a></li>
-        <li class="lien-menu "><a href="#">Gestion Inscriptions</a></li>
+        <li class="lien-menu lien-actif"><a href="admin.php">Accueil</a></li>
+        <li class="lien-menu "><a href="creationcompte.php">Gestion Inscriptions</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#" class="lien-actif">Déconnexion</a></li>
--- index.php
+++ index.php
@@ -19,33 +19,69 @@ include_once 'app/bootstrap.inc.php';
 
     <!--FontAwesome -->
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script type="text/javascript">
       $(window).load(function() {
         $('#exampleModal').modal('show');
-        
+
       });
     </script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
   </head>
   <body>
     <?php include 'include/menuIndex.php'; ?>
-    <h1><i class="fa fa-print" aria-hidden="true"></i>Bonjour</h1>
+    <header>
+     <div class="header-content">
+         <div class="header-content-inner">
+             <h1 id="homeHeading">BIENVENUE SUR REFUGEES ASSISTANCE</h1>
+             <hr>
+             <p>Afin de pouvoir profiter pleinement de notre site, choisissez la catégorie correspondant à votre situation</p>
+             <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+         </div>
+     </div>
+ </header>
     <a href="connexion.php">Organisateur</a>
     <a href="carte.php">Refugié</a>
     <div class="modal fade bs-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-          
+
         </div>
       </div>
     </div>
-    
 
 
 
 
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
   </body>
 </html>
